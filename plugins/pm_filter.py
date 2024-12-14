2498c62619a4aff52227b391e816af197af4cd8c
☬ Jack Official <161012414+Jack-Official@users.noreply.github.com>
2024-12-15 04:23:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -162,9 +162,7 @@ async def next_page(bot, query):
                 ),
             ]
             for file in files
-        ]
-        
-    )    
+        ]  
 
     if 0 < offset <= 10:
         off_set = 0
