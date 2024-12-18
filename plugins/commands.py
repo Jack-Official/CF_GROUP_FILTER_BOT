3672897cec0e66120b8c88ed6091d7ea55383ad3
☬ Jack Official <161012414+Jack-Official@users.noreply.github.com>
2024-12-18 16:11:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,8 +72,8 @@ async def start(client, message):
                 InlineKeyboardButton(
                     "ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=invite_link.invite_link
                 ),
-                InlineKeyboardButton(
-                    " ᴍᴇ ᴊᴏɪɴᴇᴅ", callback_data=f"{pre}#{file_id}")
+                InlineKeyboardButton("Hᴇʏ Bᴏᴛ....! Wʜʏ I'ᴍ ᴊᴏɪɴɪɴɢ", callback_data="neosub")
+                    
             ]
         ]
 
@@ -81,7 +81,7 @@ async def start(client, message):
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub'
-                btn.append([InlineKeyboardButton("Hᴇʏ Bᴏᴛ....! Wʜʏ I'ᴍ ᴊᴏɪɴɪɴɢ", callback_data="neosub")])
+                btn.append([InlineKeyboardButton(" ᴍᴇ ᴊᴏɪɴᴇᴅ", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton(" ᴍᴇ ᴊᴏɪɴᴇᴅ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_photo(
