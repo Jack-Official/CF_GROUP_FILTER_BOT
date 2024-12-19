02d50bcff568f3ecc51ba0be4f9d2d217ced0d7c
☬ Jack Official <161012414+Jack-Official@users.noreply.github.com>
2024-12-19 19:17:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -45,7 +45,7 @@ class script(object):
 
     CUSTOM_FILE_CAPTION = """<b>🎟️ ʀᴇϙᴜᴇsᴛᴇᴅ ʙʏ  {mention}</b>
     
-    <b>ғɪʟᴇ ɴᴀᴍᴇ :</b><b><code>{file_name}</code></b>"""
+    <b>🗃 ғɪʟᴇ ɴᴀᴍᴇ :</b><b><code>{file_name}</code></b>"""
 
     REQBEST = """♻️ ᴀsᴋ ᴍᴏᴠɪᴇs ᴄᴏʀʀᴇᴄᴛ sᴘᴇʟʟɪɴɢ\n\n♻️ ᴅᴏɴᴛ ᴀsᴋ ᴍᴏᴠɪᴇs ᴛʜᴏsᴇ ᴀʀᴇ ɴᴏᴛ ʀᴇʟᴇᴀsᴇᴅ ɪɴ ᴏᴛᴛ ᴛʜᴇᴀᴛʀᴇ ϙᴜᴀʟɪᴛʏ ᴀᴠᴀɪʟᴀʙʟᴇ 🥹\n\n♻️ ғᴏʀ ʙᴇᴛᴛᴇʀ ʀᴇsᴜʟᴛ\n\n- ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ʏᴇᴀʀ\n\n- ᴇɴɢ : ʟᴇᴏ 2023"""
 
